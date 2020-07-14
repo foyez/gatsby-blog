04250530fe2683410e28464d832d21b4dde1e312
Kazi Foyez Ahmed <foyezar@yahoo.com>
2020-07-15 04:37:08 +0600

    Update me.js

--- src/pages/me.js
+++ src/pages/me.js
@@ -9,19 +9,7 @@ const AboutPage = () => (
     <Head title="About" />
     <h1>About Me</h1>
     <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I am a web developer dedicated to learning modern JavaScript, Node.js, and development. After obtaining my Bachelor's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time.</p>
-    <p>I currently working as a web developer.</p>
+    <p>I am currently working as a web developer.</p>
     <p>
       <Link to='/contact'>Want to work with me? Reach out.</Link>
     </p>
